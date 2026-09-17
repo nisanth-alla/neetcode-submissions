0dd4171278e7fe3b7634fb2b2ce2cda91de2739d
nisanth-alla <nisanth.alla@gmail.com>
2026-09-17 19:40:12 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const anagramGroups = new Map<string, string[]>();
+        const final = [];
+
+        strs.forEach(word => {
+            const key = word.split('').sort().join('');
+
+            if(!anagramGroups.has(key)) anagramGroups.set(key, [word]);
+            else anagramGroups.get(key).push(word);
+        })
+
+        return [...anagramGroups.values()];
+    }
+}
